b4f6287891b7921e1227d0083b1c6950b56e385b
justlikemark <>
2020-03-30 11:09:38 +0800

    update wechat tweak,add awrobot source folder

--- nsforwechat/NSViewController.m
+++ nsforwechat/NSViewController.m
@@ -24,12 +24,12 @@
     if(width > window.bounds.size.height){
         width = window.bounds.size.height;
     }
-    _icon.frame = CGRectMake(window.bounds.size.width*0.65,0,width*0.14*zoom,width*0.14*zoom);
+    _icon.frame = CGRectMake(window.bounds.size.width*0.65,0,60,50);
 //    [icon setBackgroundImage:[UIImage imageNamed:@"/nsfiles/wechat/ui/icon.png"] forState:UIControlStateNormal];
-    _icon.backgroundColor = [UIColor redColor];
-    [_icon setTitle:@"抢" forState:0];
+    _icon.backgroundColor = [UIColor blackColor];
+    [_icon setTitle:@"开抢" forState:0];
     if([[[NSUserDefaults standardUserDefaults] objectForKey:@"redopen"] isEqualToString:@"YES"])
-        [_icon setTitle:@"open" forState:0];
+        [_icon setTitle:@"开启" forState:0];
     [_icon setTitleColor:[UIColor whiteColor] forState:0];
     //设置透明
     _icon.alpha = 0.8;
@@ -57,13 +57,13 @@
     if ([btnTitle isEqualToString:@"关闭"]) {
         NSLog(@"close");
         [[NSUserDefaults standardUserDefaults] setObject:@"NO" forKey:@"redopen"];
-        [_icon setTitle:@"C" forState:0];
+        [_icon setTitle:@"关闭" forState:0];
     }else if ([btnTitle isEqualToString:@"开启"] ) {
         bool bundleYes = [self getAllApp];
         if(bundleYes)
         {
             [[NSUserDefaults standardUserDefaults] setObject:@"YES" forKey:@"redopen"];
-            [_icon setTitle:@"O" forState:0];
+            [_icon setTitle:@"开启" forState:0];
         }else{
             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"没有安装app"
                                                             message:@"请添加源：http://apt.so/nashizhushou 下载那是助手app，后在使用"
